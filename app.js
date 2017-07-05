3afa61c3963e87136d64f846ede99bdd4320c27b
kelvingao <89156201@qq.com>
2017-07-05 21:46:52 +0800

    Seperate the palyer and socket.

--- app.js
+++ app.js
@@ -15,31 +15,51 @@ serv.listen(2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+class Player {
+    constructor(id) {
+        this.x = 250;
+        this.y = 250;
+        this.id = id;
+        this.number = "" + Math.floor(10 * Math.random());
+    }
+}
+// var Player = function(id) {
+//     var self = {
+//         x: 250,
+//         y: 250,
+//         id: id,
+//         number: "" + Math.floor(10 * Math.random())
+//     }
+//     return self;
+//}
 
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket) {
     console.log('socket connection');
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = "" + Math.floor(10 * Math.random());
     SOCKET_LIST[socket.id] = socket;
+
+    var player = new Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
 
     socket.on('disconnect', function() {
         delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
     })
 });
 
 setInterval(function(){
     var pack = [];
-    for (var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST[i];
-        socket.x++;
-        socket.y++;
+    for (var i in PLAYER_LIST) {
+        var player = PLAYER_LIST[i];
+        player.x++;
+        player.y++;
         pack.push({
-            x:socket.x,
-            y:socket.y,
-            number:socket.number
+            x:player.x,
+            y:player.y,
+            number:player.number
         });
     }
     for (var i in SOCKET_LIST) {

bcdf4bff37069bda6c45477a1b51a2d4bff980c5
kelvingao <89156201@qq.com>
2017-07-06 14:38:39 +0800

    Add Bullet random shooting

--- app.js
+++ app.js
@@ -100,8 +100,6 @@ io.sockets.on('connection', function(socket) {
         delete SOCKET_LIST[socket.id];
         Player.onDisconnect(socket);
     })
-
-    
 });
 
 Player.update = function() {
@@ -118,10 +116,48 @@ Player.update = function() {
     return pack;
 }
 
-setInterval(function(){
-    var pack = Player.update();
+var Bullet = function(angle) {
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
 
-    for (var i in SOCKET_LIST) {
+    self.timer = 0;
+    self.toRemove = false;
+    var super_update = self.update;
+    self.update = function() {
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+    }
+    Bullet.list[self.id] = self;
+    return self;
+}
+
+Bullet.list = {};
+Bullet.update = function() {
+    if(Math.random() < 0.1) {
+        Bullet(Math.random()*360);
+    }
+    var pack = [];
+    for (var i in Bullet.list) {
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        });
+    }
+    return pack;
+}
+
+setInterval(function(){
+    var pack = {
+        player: Player.update(),
+        bullet: Bullet.update(),
+    }
+
+    for (var i in SOCKET_LIST) {    
         var socket = SOCKET_LIST[i];
         socket.emit('newPosition', pack);
     }

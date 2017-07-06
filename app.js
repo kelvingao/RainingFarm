ca13c07e38d639fb3ed2b520c4e360db576d7348
kelvingao <89156201@qq.com>
2017-07-06 20:24:05 +0800

    Player attacks

--- app.js
+++ app.js
@@ -43,12 +43,23 @@ var Player = function(id) {
     self.pressingLeft = false,
     self.pressingUp = false,
     self.pressingDown = false,
+    self.pressingAttack = false,
+    self.mouseAngle = 0,
     self.maxSpd = 10;
 
     var super_update = self.update; // save it
     self.update = function() {
         self.updateSpd();
         super_update();
+
+        if(self.pressingAttack) {
+           self.shootBullet(self.mouseAngle);
+        }
+    }
+    self.shootBullet = function(angle) {
+        var b = Bullet(angle);
+        b.x = self.x;
+        b.y = self.y;
     }
 
     self.updateSpd = function() {
@@ -82,6 +93,10 @@ Player.onConnect = function(socket) {
             player.pressingUp = data.state;
         else if(data.inputId == 'down')
             player.pressingDown = data.state;
+        else if(data.inputId == 'attack')
+            player.pressingAttack = data.state;
+        else if(data.inputId == 'mouseAngle')
+            player.mouseAngle = data.state;
     });
 }
 
@@ -151,9 +166,7 @@ var Bullet = function(angle) {
 
 Bullet.list = {};
 Bullet.update = function() {
-    if(Math.random() < 0.1) {
-        Bullet(Math.random()*360);
-    }
+
     var pack = [];
     for (var i in Bullet.list) {
         var bullet = Bullet.list[i];

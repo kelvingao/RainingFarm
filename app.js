70a0a3d844ee86a04bb33360275718cca79885a5
kelvingao <89156201@qq.com>
2017-07-07 10:53:50 +0800

    Sign in and Sign up

--- app.js
+++ app.js
@@ -112,6 +112,21 @@ Player.onDisconnect = function(socket) {
 
 var DEBUG = true;
 
+var USERS = {
+    //username:password
+    "bob":"asd",
+    "bob2":"bob",
+    "bob3":"ttt",
+}
+var isValidPassword = function(data) {
+    return USERS[data.username] === data.password;
+}
+var isUsernameTaken = function(data) {
+    return USERS[data.username];
+}
+var addUser = function(data) {
+    USERS[data.username] = data.password;
+}
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket) {
     console.log("Start connection.");
@@ -119,14 +134,22 @@ io.sockets.on('connection', function(socket) {
     SOCKET_LIST[socket.id] = socket;
 
     socket.on('signIn', function(data) {
-        console.log("server listen signIn");
-        if(data.username === 'bob' && data.password === 'asd') {
+        if(isValidPassword(data)) {
             Player.onConnect(socket);
             socket.emit('signInResponse', {success:true});
         } else {
             socket.emit('signInResponse', {success:false});
         }
     });
+
+    socket.on('signUp', function(data) {
+        if(isUsernameTaken(data))
+            socket.emit('signUpResponse', {success:false});
+        else {
+            addUser(data);
+            socket.emit('signUpResponse', {success:true});
+        }
+    })
 
     socket.on('disconnect', function() {
         delete SOCKET_LIST[socket.id];
